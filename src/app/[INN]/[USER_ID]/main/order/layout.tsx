d9e2f817bb07acc059a019302662eda17dd3bdab
pavel <paveldpv91@gmail.com>
2026-01-27 16:22:01 +0300

    feat: add dynamic order routes and types
    - Add dynamic routes: /[INN]/[USER_ID]/main/order/
    - Add TypeScript types in src/types/
    - Update main page layout
    - Update logo

---
+++ src/app/[INN]/[USER_ID]/main/order/layout.tsx
@@ -0,0 +1,17 @@
+import { Suspense } from "react";
+
+export default function layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Suspense
+        fallback={
+          <div className=" flex justify-center items-center">
+            <span className="Loader">Загружаем</span>
+          </div>
+        }
+      >
+        {children}
+      </Suspense>
+    </>
+  );
+}
